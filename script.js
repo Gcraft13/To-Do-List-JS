c433084d1ecabe36c1e342d0c71c9515bae5e948
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-22 09:50:03 -0400

    add code to generate current date and populate it on the html

---
+++ script.js
@@ -0,0 +1,7 @@
+//Setting the current date
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let currentDate = `${month}/${day}/${year}`;
+$(".current-date").text(currentDate);
